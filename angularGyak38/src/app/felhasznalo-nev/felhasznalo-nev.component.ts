cb9746ac5d13fc0427a3967314a409de53d31b77
nemethbarbo <nemethbarbo@gmail.com>
2023-06-02 00:07:56 +0200

    felhasznalonev feladat

--- angularGyak38/src/app/felhasznalo-nev/felhasznalo-nev.component.ts
+++ angularGyak38/src/app/felhasznalo-nev/felhasznalo-nev.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./felhasznalo-nev.component.css']
 })
 export class FelhasznaloNevComponent {
+  private felhasznalonev: string = "" ;
 
+  public get Felhasznalonev(): string {
+    return this.felhasznalonev;
+  }
+
+  public set Felhasznalonev(v: string){
+    this.felhasznalonev = v;
+  }
+
+  public felhasznalonevEllenoriz(felhasznalonev:string):any {
+    let regexFelhasznalonev = /^[a-zA-Z0-9_]{4,15}$/;
+    return regexFelhasznalonev.test(felhasznalonev);
+  }
+
+  public felhasznalonevMegfeleloE():void{
+    if (this.felhasznalonevEllenoriz(this.felhasznalonev)){
+      alert("A felhasználónév formátuma megfelelő!");
+    } else {
+      alert ("A felhasználónév formátuma nem megfelelő (nem tartalmazhat speciális karaktereket csak alulvonást, számot vagy angol karaktert, a hossza minimum 4, maximum 15 karakter lehet!)");
+    }
+  }
 }
